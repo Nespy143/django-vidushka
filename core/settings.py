6588c8777b1474438f34dd7fadae9766e19f9fcd
Daniil <daniilk23567@gmail.com>
2024-09-17 10:00:48 +0300

    Add video

--- core/settings.py
+++ core/settings.py
@@ -82,14 +82,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'ru'
-
 TIME_ZONE = 'Europe/Moscow'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
+
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / '/static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
