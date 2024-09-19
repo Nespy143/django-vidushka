372e7731fcb93437625de00d06838e77ebb795a8
Dan <daniilk23567@gmail.com>
2024-09-19 09:56:26 +0300

    NEWnew

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from videos import models
 
 def home_page(request: HttpRequest ) -> HttpResponse:
-    return HttpResponse("Home page")
+    videos = models.Video.objects.all()
+    context = {'videos': videos}
+    return render(request, 'main/index.html', context)
